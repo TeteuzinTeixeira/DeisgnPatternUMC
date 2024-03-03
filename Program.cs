fd1e1dbf4ae0a51399ae12859110f1b5dfda38af
iagoSilvaMuralis <iago.silva@muralis.com.br>
2024-03-03 18:00:30 -0300

    add menu main

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Aula01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int opcao = 0;
+        Aluno aluno = new Aluno();
+        Livro livro = new Livro();
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("------------ Sistema de Gerenciamento ------------\n");
+            Console.WriteLine("Escolha a opcao desejada\n");
+            Console.WriteLine("1. Aluno\n");
+            Console.WriteLine("2. Livro\n");
+            Console.WriteLine("0. Sair\n");
+
+            Console.Write("Opcao: ");
+            opcao = int.Parse(Console.ReadLine());
+            switch (opcao)
+            {
+                case 1:
+                    aluno.AlunoMenu(); 
+                    break;
+                case 2:
+                    livro.LivroMenu();
+                    break;
+                case 0:
+                    Console.WriteLine("Feito por: \nMATEUS TEIXEIRA GOMES e IAGO DA SILVA LIMA");
+                    return;
+                
+            }
+
+
+
+        } while (opcao != 0);
+            
+
+    }
+}
